f20fc7d5ed5b86618f326fb0d8892a91606ecfd4
Aziza Abdurakhmonova <aziza_abdurakhmonova@epam.com>
2023-06-19 11:47:19 +0500

    Catalog.DAL.Implementation is implemented

---
+++ Catalog.API/Catalog.DAL.Implementation/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.DAL.Contracts.Entities;
+
+namespace Catalog.DAL.Implementation
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<ItemEntity> Items { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+
+        public DbSet<T> DbSet<T>() where T : class
+        {
+            return Set<T>();
+        }
+
+        public new IQueryable<T> Query<T>() where T : class
+        {
+            return Set<T>();
+        }
+    }
+}
---
+++ Catalog.API/Catalog.DAL.Implementation/DbRepository.cs
@@ -0,0 +1,81 @@
+﻿using Catalog.DAL.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.DAL.Contracts.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Security.Principal;
+
+namespace Catalog.DAL.Implementation
+{
+    public class DbRepository: IDbRepository
+    {
+        private readonly DataContext _context;
+
+        public DbRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Get<T>() where T : class
+        {
+            return _context.Set<T>().AsQueryable();
+        }
+
+        public IQueryable<T> Get<T>(Expression<Func<T, bool>> selector) where T : class
+        {
+            return _context.Set<T>().Where(selector).AsQueryable();
+        }
+
+        public async Task<Guid> Add<T>(T newEntity) where T : class, IEntitry
+        {
+            var entity = await _context.Set<T>().AddAsync(newEntity);
+            return entity.Entity.Id;
+        }
+
+        public async Task AddRange<T>(IEnumerable<T> newEntities) where T : class
+        {
+            await _context.Set<T>().AddRangeAsync(newEntities);
+        }
+
+        public async Task Delete<T>(Guid id) where T : class 
+        {
+            var activeEntity = await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+            activeEntity.IsActive = false;
+            await Task.Run(() => _context.Update(activeEntity));
+        }
+
+        public async Task Remove<T>(T entity) where T : class
+        {
+            await Task.Run(() => _context.Set<T>().Remove(entity));
+        }
+
+        public async Task RemoveRange<T>(IEnumerable<T> entities) where T : class
+        {
+            await Task.Run(() => _context.Set<T>().RemoveRange(entities));
+        }
+
+        public async Task Update<T>(T entity) where T : class
+        {
+            await Task.Run(() => _context.Set<T>().Update(entity));
+        }
+
+        public async Task UpdateRange<T>(IEnumerable<T> entities) where T : class
+        {
+            await Task.Run(() => _context.Set<T>().UpdateRange(entities));
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<T> GetAll<T>() where T : class
+        {
+            return _context.Set<T>().AsQueryable();
+        }
+    }
+}
